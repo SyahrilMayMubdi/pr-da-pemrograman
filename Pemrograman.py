4d7aa478c86201ad0627fcfe4bc3b51414a900ac
SyahrilMayMubdi <140218624+SyahrilMayMubdi@users.noreply.github.com>
2023-08-17 19:46:57 +0700

    Add files via upload

---
+++ Pemrograman.py
@@ -0,0 +1,4 @@
+print("BIODATA SYAHRIL")
+print("Nama : Syahril May Mubdi")
+print("Kelas : X-RPL1")
+print("Di RPL Ingin Jadi Apa : Saya ingin mendapatkan banyak ilmu pemrograman sehingga suatu saat saya bisa menciptakan suatu aplikasi atau game dan menjadi mewujudkan cita cita saya sebagai seorang game developer")
